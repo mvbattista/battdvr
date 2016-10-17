2724c3604ca1541c31cc13f658b8cb519355486d
Michael V. Battista <github@mvbattista.com>
2016-10-17 07:00:38 -0400

    Add NBC Processor, TVMaze API, and main function layout

--- battdvr.py
+++ battdvr.py
@@ -25,32 +25,65 @@ supergirl = 'http://www.cwtv.com/shows/supergirl/'
 
 gotham = 'http://www.fox.com/gotham/full-episodes'
 family_guy = 'http://www.fox.com/family-guy/full-episodes'
+new_girl = 'http://www.fox.com/new-girl/full-episodes'
 lucifer = 'http://www.fox.com/lucifer/full-episodes'
 pitch = 'http://www.fox.com/pitch/full-episodes'
 son_of_zorn = 'http://www.fox.com/son-of-zorn/full-episodes'
 brooklyn_99 = 'http://www.fox.com/brooklyn-nine-nine/full-episodes'
+bobs_burgers = 'http://www.fox.com/bobs-burgers/full-episodes'
+simpsons = 'http://www.fox.com/the-simpsons/full-episodes'
 
 tbbt = 'http://www.cbs.com/shows/big_bang_theory/video/'
 tbg = 'http://www.cbs.com/shows/2_broke_girls/video/'
 kcw = 'http://www.cbs.com/shows/kevin-can-wait/video/'
+scorpion = 'http://www.cbs.com/shows/scorpion/video/'
+odd_couple = 'http://www.cbs.com/shows/the-odd-couple/video/'
+mom = 'http://www.cbs.com/shows/mom/video/'
+macgyver = 'http://www.cbs.com/shows/macgyver/video/'
 
 blackish = 'http://abc.go.com/shows/blackish/episode-guide'
+middle = 'http://abc.go.com/shows/the-middle/episode-guide'
+american_housewife = 'http://abc.go.com/shows/american-housewife/episode-guide'
+modern_family = 'http://abc.go.com/shows/modern-family/episode-guide'
+shield = 'http://abc.go.com/shows/marvels-agents-of-shield/episode-guide'
+
+superstore = 'http://www.nbc.com/superstore/episodes'
+timeless = 'http://www.nbc.com/timeless/episodes'
+good_place = 'http://www.nbc.com/the-good-place/episode'
 
 all_shows = [
 {'network': 'cw', 'url': lot, 'show_name':'Legends of Tomorrow'},
 {'network': 'cw', 'url': arrow, },
 {'network': 'cw', 'url': flash, 'series_directory':'The Flash (2014)', },
 {'network': 'cw', 'url': supergirl, },
+
 {'network': 'cbs', 'url': tbbt},
 {'network': 'cbs', 'url': tbg},
 {'network': 'cbs', 'url': kcw},
+{'network': 'cbs', 'url': scorpion},
+{'network': 'cbs', 'url': odd_couple},
+{'network': 'cbs', 'url': mom},
+{'network': 'cbs', 'url': macgyver, 'series_directory':'MacGyver (2016)', },
+
 {'network': 'abc', 'url': blackish, 'show_name':'Blackish'},
+{'network': 'abc', 'url': middle,},
+{'network': 'abc', 'url': american_housewife,},
+{'network': 'abc', 'url': modern_family,},
+{'network': 'abc', 'url': shield, 'series_directory': 'Marvel/Agents of S.H.I.E.L.D.'},
+
 {'network': 'fox', 'url': gotham, 'show_name':'Gotham'},
+{'network': 'fox', 'url': new_girl, 'show_name':'New Girl'},
 {'network': 'fox', 'url': family_guy, 'show_name':'Family Guy'},
 {'network': 'fox', 'url': lucifer, 'show_name':'Lucifer'},
 {'network': 'fox', 'url': pitch, 'show_name':'Pitch'},
 {'network': 'fox', 'url': son_of_zorn, 'show_name':'Son of Zorn'},
 {'network': 'fox', 'url': brooklyn_99, 'show_name':'Brooklyn Nine Nine'},
+{'network': 'fox', 'url': bobs_burgers, 'show_name':"Bob's Burgers"},
+{'network': 'fox', 'url': simpsons, 'show_name':'The Simpsons'},
+
+{'network': 'nbc', 'url': superstore, 'show_name': 'Superstore'},
+{'network': 'nbc', 'url': timeless, 'show_name': 'Timeless'},
+{'network': 'nbc', 'url': good_place, 'show_name': 'The Good Place'},
 ]
 
 # youtube_dl configuration
@@ -92,18 +125,48 @@ def ytdl_hook(d):
 
 # download_format = 'youtube-dl -o "%(series)s - %(season_number)sx%(episode_number)s - %(title)s.%(ext)s" '
 
-for show in all_shows:
+def download_all():
+    downloaded_urls = []
+    skipped_urls = []
+    for show in all_shows:
+        constructor = PROCESSOR_FOR[show['network']]
+        print('[{}]'.format(show['url']))
+        show_processor = constructor(**show)
+        # show_processor.load_show(**show)
+        episodes = show_processor.get_links(show['url'])
+        for episode in episodes:
+            try:
+                dest = show_processor.get_filename(episode)
+                downloaded = show_processor.download(episode, dest)
+            except youtube_dl.utils.DownloadError as e:
+                print ('Cannot download {} due to network limitations.'.format(episode))
+                skipped_urls.append(episode)
+                continue
+            if downloaded:
+                downloaded_urls.append(episode)
+            else:
+                skipped_urls.append(episode)
+    print('{} downloaded, {} skipped'.format(len(downloaded_urls), len(skipped_urls)))
+
+def check_links(show):
     constructor = PROCESSOR_FOR[show['network']]
     print('[{}]'.format(show['url']))
-    show_processor = constructor()
-    show_processor.load_show(**show)
+    show_processor = constructor(**show)
+    episodes = show_processor.get_links(show['url'])
+    pprint(episodes)
+
+def check_filenames(show):
+    constructor = PROCESSOR_FOR[show['network']]
+    print('[{}]'.format(show['url']))
+    show_processor = constructor(**show)
     episodes = show_processor.get_links(show['url'])
     for episode in episodes:
-        try:
-            dest = show_processor.get_filename(episode)
-        except youtube_dl.utils.DownloadError as e:
-            print ('Cannot download {} due to network limitations.'.format(episode))
-        show_processor.download(episode, dest)
+        dest = show_processor.get_filename(episode)
+        print(dest)
 
 
+if __name__ == '__main__':
+    download_all()
+    # check_links({'network': 'nbc', 'url': superstore})
+    # check_filenames({'network': 'nbc', 'url': superstore, 'show_name': 'Superstore',})
 
--- networks.py
+++ networks.py
@@ -2,10 +2,12 @@ import os
 from pprint import pprint
 from urllib.parse import urljoin
 import json
-from difflib import SequenceMatcher
+# from difflib import SequenceMatcher
 
 import requests
 import youtube_dl
+import Levenshtein
+from fuzzywuzzy import fuzz, process
 
 from bs4 import BeautifulSoup
 
@@ -15,22 +17,24 @@ HOME_DIRECTORY = '/Volumes/Public/Video/TV Shows/'
 class BaseNetwork(object):
     """docstring for base network"""
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         self.show_name = None
         self.series_directory = None
         self.has_season = True
         self.has_episode_number = True
         self.verbose = False
         self.data_from_tvmaze = False
+        self.tvmaze_show_id = None
+        self.tvmaze_episode_data = None
 
         # self.__dict__.update(kwargs)
 
-    def load_show(self, **kwargs):
+    # def load_show(self, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def get_links(self, url):
-        print('This is the get_links function')
+        raise NotImplementedError('get_links required for all processors.')
 
     def rename_title(self, title):
         return title
@@ -38,43 +42,46 @@ class BaseNetwork(object):
     def get_filename(self, url):
         with youtube_dl.YoutubeDL({'quiet':True}) as ydl:
             ydl_info = ydl.extract_info(url, download=False)
-            if self.show_name:
-                show = self.show_name
-            else:
-                show = ydl_info['series']
+            if self.verbose:
+                pprint(ydl_info)
+            
+            show = self.show_name if self.show_name else ydl_info['series']
+
             title = ydl_info['title']
             title = self.rename_title(title)
 
+            series_dir = self.series_directory if self.series_directory else show
+
             # TODO - Add the API 
-
-            if self.series_directory:
-                series_dir = self.series_directory
-            else:
-                series_dir = show
-
-            if self.verbose:
-                pprint(ydl_info)
-
-            if self.has_season:
-                season_number = ydl_info['season_number']
+            if self.data_from_tvmaze:
+                episode_info = self._get_tvmaze_data(show, title)
+                season_number = episode_info['season']
                 season_dir = 'Season {}'.format(season_number)
-            if self.has_episode_number:
-                season_episode_number = str(ydl_info['episode_number']).zfill(2)
+                season_episode_number = '00' if episode_info['number'] is None else str(episode_info['number']).zfill(2)
+
+            else:
+                if self.has_season:
+                    season_number = ydl_info['season_number']
+                    season_dir = 'Season {}'.format(season_number)
+                if self.has_episode_number:
+                    season_episode_number = str(ydl_info['episode_number']).zfill(2)
             extension = ydl_info['ext']
 
-        if self.has_season and self.has_episode_number:
+        # if self.has_season and self.has_episode_number:
             filename = "{} - {}x{} - {}.{}".format(show, season_number, season_episode_number, title, extension)
             result = os.path.join(HOME_DIRECTORY, series_dir, season_dir, filename)
-        else:
-            filename = "{} - {}.{}".format(show, title, extension)
-            result = os.path.join(HOME_DIRECTORY, series_dir, filename)
-        return result
+        # else:
+        #     self._get_tvmaze_data(show, title)
+        #     filename = "{} - {}.{}".format(show, title, extension)
+        #     result = os.path.join(HOME_DIRECTORY, series_dir, filename)
+            return result
 
-        # print('This is the get_filename function')
+        print('This is the get_filename function')
 
     def download(self, url, full_path):
         if os.path.exists(full_path):
             print('skipping {}'.format(full_path))
+            return False
         else:
             print('DOWNLOAD: {} from {}'.format(full_path, url))
             while True:
@@ -84,28 +91,47 @@ class BaseNetwork(object):
                     break
                 except youtube_dl.utils.DownloadError as e:
                     print('Ran into {}, trying again...'.format(e))
+            return True
 
 
     def _get_tvmaze_data(self, show, title):
         show_r = requests.get('http://api.tvmaze.com/search/shows', params={'q':show})
         show_search_data = json.loads(show_r.text)
-        show_id = None
-        for i in show_search_data:
-            name_match_ratio = SequenceMatcher(None, show.lower(), show_search_data[i]['show']['name'].lower()).ratio()
-            if name_match_ratio > 0.9:
-                show_id = show_search_data[i]['show']['id']
-                break
-        if show_id is None:
-            raise ValueError('Could not match TV show data')
+        best_show_ratio = 0
+        best_show_name = None
+        if not self.tvmaze_show_id:
+            for i in show_search_data:
+                show_ratio = fuzz.ratio(show, i['show']['name'])
+                if  show_ratio > best_show_ratio:
+                # name_match_ratio = SequenceMatcher(None, show.lower(), show_search_data[i]['show']['name'].lower()).ratio()
+                # if name_match_ratio > 0.9:
+                    self.tvmaze_show_id = i['show']['id']
+                    best_show_ratio = show_ratio
+                    best_show_name = i['show']['name']
+                    break
+            if self.tvmaze_show_id is None:
+                raise ValueError('Could not match TV show data')
+            print('I believe the show name is {}'.format(best_show_name))
 
-        episode_r = requests.get('http://api.tvmaze.com/shows/{}/episodes'.format(show_id))
-        # TODO - Complete this for FOX
-
+        if not self.tvmaze_episode_data:
+            episode_r = requests.get('http://api.tvmaze.com/shows/{}/episodes?specials=1'.format(self.tvmaze_show_id))
+            self.tvmaze_episode_data = json.loads(episode_r.text)
+        best_ratio = 0
+        best_obj = None
+        for i in self.tvmaze_episode_data:
+            episode_ratio = fuzz.ratio(title, i['name'])
+            # print('{} - {} = {}'.format(title, i['name'], episode_ratio))
+            if episode_ratio > best_ratio:
+                best_ratio = episode_ratio
+                best_obj = i
+        print('I believe the episode name is {} (s{} e{})'.format(best_obj['name'], best_obj['season'], best_obj['number']))
+        return best_obj
 
 
 class CWProcessor(BaseNetwork):
     """docstring for CW"""
     tld = 'http://www.cwtv.com/'
+    network = 'The CW'
 
     def get_links(self, url):
         r = requests.get(url)
@@ -121,6 +147,7 @@ class CWProcessor(BaseNetwork):
 class CBSProcessor(BaseNetwork):
     """docstring for CBS"""
     tld = 'http://www.cbs.com/'
+    network = 'CBS'
 
     def get_links(self, url):
         r = requests.get(url)
@@ -144,13 +171,29 @@ class CBSProcessor(BaseNetwork):
                 break
         if video_section_id is None:
             return []
-        episodes_url = urljoin(self.tld, '/carousels/videosBySection/{}/offset/0/limit/15/xs/0/'.format(video_section_id))
-        episodes_r = requests.get(episodes_url)
 
-        episodes_data = json.loads(episodes_r.text)
+        offset = 0
+        page_limit = 15
+        first_loop = True
+        total_episodes = 0
         result = []
-        for episode in episodes_data['result']['data']:
-            result.append(urljoin(self.tld,episode['url']))
+        while True:
+            episodes_url = urljoin(self.tld, 
+                '/carousels/videosBySection/{}/offset/{}/limit/{}/xs/0/'.format(video_section_id, offset, page_limit))
+            episodes_r = requests.get(episodes_url)
+            episodes_data = json.loads(episodes_r.text)
+
+            if first_loop:
+                total_episodes = episodes_data['result']['total']
+            for episode in episodes_data['result']['data']:
+                if not episode['is_paid_content']:
+                    result.append(urljoin(self.tld,episode['url']))
+
+            if offset + page_limit < total_episodes:
+                offset += page_limit
+                first_loop = False
+            else:
+                break
         return result
 
     def rename_title(self, title):
@@ -160,11 +203,15 @@ class CBSProcessor(BaseNetwork):
 class FOXProcessor(BaseNetwork):
     """docstring for FOX"""
     tld = 'http://www.fox.com/'
+    network = 'FOX'
 
-    def __init__(self):
-        super(FOXProcessor, self).__init__()
+    def __init__(self, **kwargs):
+        super(FOXProcessor, self).__init__(**kwargs)
         self.has_season = False
         self.has_episode_number = False
+        self.data_from_tvmaze = True
+        if not self.show_name:
+            raise ValueError('show_name required for FOX shows')
 
     @staticmethod
     def __uniq(seq):
@@ -173,8 +220,6 @@ class FOXProcessor(BaseNetwork):
         return [x for x in seq if not (x in seen or seen_add(x))]
 
     def get_links(self, url):
-        if self.show_name is None:
-            raise ValueError('show_name is required for FOXProcessor')
         r = requests.get(url)
         data = r.text
         soup = BeautifulSoup(data, 'lxml')
@@ -190,6 +235,7 @@ class FOXProcessor(BaseNetwork):
 class ABCProcessor(BaseNetwork):
     """docstring for ABC"""
     tld = 'http://abc.go.com/'
+    network = 'ABC'
 
     def get_links(self, url):
         r = requests.get(url)
@@ -204,9 +250,34 @@ class ABCProcessor(BaseNetwork):
         return result
 
 
+class NBCProcessor(BaseNetwork):
+    """docstring for NBC"""
+    tld = 'http://www.nbc.com'
+    network = 'NBC'
+
+    def __init__(self, **kwargs):
+        super(NBCProcessor, self).__init__(**kwargs)
+        self.has_season = False
+        self.has_episode_number = False
+        self.data_from_tvmaze = True
+        if not self.show_name:
+            raise ValueError('show_name required for NBC shows.')
+
+    def get_links(self, url):
+        r = requests.get(url)
+        data = r.text
+        soup = BeautifulSoup(data, 'lxml')
+        result = []
+        for episode in soup.find_all('article'):
+            link = episode.find('a')
+            result.append(urljoin(self.tld, link.get('href')))
+
+        return result
+
 PROCESSOR_FOR = {
     'cw': CWProcessor,
     'cbs': CBSProcessor,
     'abc': ABCProcessor,
     'fox': FOXProcessor,
+    'nbc': NBCProcessor,
 }
