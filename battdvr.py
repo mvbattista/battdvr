b6e104e9c44f4fd25c0fc2e10672530b82bbca83
Michael V. Battista <github@mvbattista.com>
2017-01-01 20:30:14 -0500

    Add setup.py and config preferences

--- battdvr.py
+++ battdvr.py
@@ -12,79 +12,34 @@ from networks import *
 
 import importlib
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+__author__ = 'Michael V. Battista'
+__copyright__ = 'Copyright 2016, Open Source'
+__credits__ = []
+__license__ = 'GPL'
+__version__ = '0.7.0'
+__maintainer__ = "Michael V. Battista"
+__email__ = "battdvr@mvbattista.com"
+__status__ = "Development"
+__appname__ = 'battdvr'
 
-lot = 'http://www.cwtv.com/shows/dcs-legends-of-tomorrow/'
-flash = 'http://www.cwtv.com/shows/the-flash/'
-arrow = 'http://www.cwtv.com/shows/arrow/'
-supergirl = 'http://www.cwtv.com/shows/supergirl/'
 
-gotham = 'http://www.fox.com/gotham/full-episodes'
-family_guy = 'http://www.fox.com/family-guy/full-episodes'
-new_girl = 'http://www.fox.com/new-girl/full-episodes'
-lucifer = 'http://www.fox.com/lucifer/full-episodes'
-pitch = 'http://www.fox.com/pitch/full-episodes'
-son_of_zorn = 'http://www.fox.com/son-of-zorn/full-episodes'
-brooklyn_99 = 'http://www.fox.com/brooklyn-nine-nine/full-episodes'
-bobs_burgers = 'http://www.fox.com/bobs-burgers/full-episodes'
-simpsons = 'http://www.fox.com/the-simpsons/full-episodes'
+def get_prefs():
+    result = []
+    possible_conf_locations = [ 
+        os.path.join(os.path.expanduser('~'), '.config', __appname__, 'config'),
+        os.path.join(os.path.expanduser('~'), '.config', __appname__ + '.conf')
+    ]
+    found_conf_loc = None
+    for loc in possible_conf_locations:
+        if os.path.isfile(loc):
+            found_conf_loc = loc
+            break
+    if found_conf_loc:
+        with open(found_conf_loc, 'r') as f:
+            full_result = f.read()
+        result = json.loads(full_result)
+    return result
 
-tbbt = 'http://www.cbs.com/shows/big_bang_theory/video/'
-tbg = 'http://www.cbs.com/shows/2_broke_girls/video/'
-kcw = 'http://www.cbs.com/shows/kevin-can-wait/video/'
-scorpion = 'http://www.cbs.com/shows/scorpion/video/'
-odd_couple = 'http://www.cbs.com/shows/the-odd-couple/video/'
-mom = 'http://www.cbs.com/shows/mom/video/'
-macgyver = 'http://www.cbs.com/shows/macgyver/video/'
-
-blackish = 'http://abc.go.com/shows/blackish/episode-guide'
-middle = 'http://abc.go.com/shows/the-middle/episode-guide'
-american_housewife = 'http://abc.go.com/shows/american-housewife/episode-guide'
-modern_family = 'http://abc.go.com/shows/modern-family/episode-guide'
-shield = 'http://abc.go.com/shows/marvels-agents-of-shield/episode-guide'
-
-superstore = 'http://www.nbc.com/superstore/episodes'
-timeless = 'http://www.nbc.com/timeless/episodes'
-good_place = 'http://www.nbc.com/the-good-place/episodes'
-
-all_shows = [
-{'network': 'cw', 'url': lot, 'show_name':'Legends of Tomorrow'},
-{'network': 'cw', 'url': arrow, },
-{'network': 'cw', 'url': flash, 'series_directory':'The Flash (2014)', },
-{'network': 'cw', 'url': supergirl, },
-
-{'network': 'cbs', 'url': tbbt},
-{'network': 'cbs', 'url': tbg},
-{'network': 'cbs', 'url': kcw},
-{'network': 'cbs', 'url': scorpion},
-{'network': 'cbs', 'url': odd_couple},
-{'network': 'cbs', 'url': mom},
-{'network': 'cbs', 'url': macgyver, 'series_directory':'MacGyver (2016)', },
-
-{'network': 'abc', 'url': blackish, 'show_name':'Blackish'},
-{'network': 'abc', 'url': middle,},
-{'network': 'abc', 'url': american_housewife,},
-{'network': 'abc', 'url': modern_family,},
-{'network': 'abc', 'url': shield, 'series_directory': 'Marvel/Agents of S.H.I.E.L.D.'},
-
-{'network': 'fox', 'url': gotham, 'show_name':'Gotham'},
-{'network': 'fox', 'url': new_girl, 'show_name':'New Girl'},
-{'network': 'fox', 'url': family_guy, 'show_name':'Family Guy'},
-{'network': 'fox', 'url': lucifer, 'show_name':'Lucifer'},
-{'network': 'fox', 'url': pitch, 'show_name':'Pitch'},
-{'network': 'fox', 'url': son_of_zorn, 'show_name':'Son of Zorn'},
-{'network': 'fox', 'url': brooklyn_99, 'show_name':'Brooklyn Nine Nine'},
-{'network': 'fox', 'url': bobs_burgers, 'show_name':"Bob's Burgers"},
-{'network': 'fox', 'url': simpsons, 'show_name':'The Simpsons'},
-
-{'network': 'nbc', 'url': superstore, 'show_name': 'Superstore'},
-{'network': 'nbc', 'url': timeless, 'show_name': 'Timeless'},
-{'network': 'nbc', 'url': good_place, 'show_name': 'The Good Place'},
-]
 
 # youtube_dl configuration
 class ytdl_logger(object):
@@ -128,6 +83,7 @@ def ytdl_hook(d):
 def download_all():
     downloaded_urls = []
     skipped_urls = []
+    all_shows = get_prefs()
     for show in all_shows:
         constructor = PROCESSOR_FOR[show['network']]
         print('[{}]'.format(show['url']))
@@ -147,6 +103,7 @@ def download_all():
             else:
                 skipped_urls.append(episode)
     print('{} downloaded, {} skipped'.format(len(downloaded_urls), len(skipped_urls)))
+    print('DOWNLOADED:\n', downloaded_urls)
 
 def check_links(show):
     constructor = PROCESSOR_FOR[show['network']]
